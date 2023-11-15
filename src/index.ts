6687fda3a5845c0917ca3822506e4f3e7a9995be
ssbarbee <ssbarbee@users.noreply.github.com>
2023-11-15 16:39:44 +0100

    feat: export and add wagmi, viewem

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export * from './components/Web3Wallet';
+export * from './hooks/useWalletAccount';
+export * from './hooks/useWalletConnect';
+export * from './providers/WalletConfig';
+export * from './setup';
