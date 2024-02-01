59e5c8670f576f8e1ef43c7f0e8e176794b36c58
ssbarbee <ssbarbee@users.noreply.github.com>
2024-02-01 23:55:39 +0100

    feat: add error

--- src/hooks/useChain.ts
+++ src/hooks/useChain.ts
@@ -1,3 +1,4 @@
+import { SwitchChainErrorType } from '@wagmi/core';
 import { useChainId, useSwitchChain } from 'wagmi';
 
 type UseChainResult = {
@@ -5,10 +6,11 @@ type UseChainResult = {
   isErrorSwitching: boolean;
   isSwitching: boolean;
   switchChain: (id: number) => void;
+  error: SwitchChainErrorType | null;
 };
 
 export const useChain = (): UseChainResult => {
-  const { switchChain, isError, isPending } = useSwitchChain();
+  const { switchChain, error, isError, isPending } = useSwitchChain();
   const chainId = useChainId();
   const handleSwitchChain = (id: number) => {
     switchChain({ chainId: id });
@@ -16,6 +18,7 @@ export const useChain = (): UseChainResult => {
 
   return {
     chainId,
+    error,
     isErrorSwitching: isError,
     isSwitching: isPending,
     switchChain: handleSwitchChain,
