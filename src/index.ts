2f37d4977b9d8fd7778b39a71d2c5d027b04fbb4
ssbarbee <ssbarbee@users.noreply.github.com>
2024-05-18 11:27:39 +0200

    fix: reexport types

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './hooks/useChain';
 export * from './hooks/useWalletAccount';
 export * from './hooks/useWalletConnect';
 export * from './providers/WalletConfig';
+export * from './providers/WalletConfig/types';
 export * from './setup';
